38e69516aa6c313aab86a19bacb4552f74437da0
Shahrukh <shahrukh25795@gmail.com>
2019-11-03 11:44:26 +0530

    fetch git user list on search

--- src/Components/GitHubSigleUsrDetail/GitHubSigleUsrDetail.jsx
+++ src/Components/GitHubSigleUsrDetail/GitHubSigleUsrDetail.jsx
@@ -1,24 +1,68 @@
 import React from 'react';
+import AsyncSelect from 'react-select/async';
+import {getApi} from '../Communication/Communication'
+
+var debounce = require('debounce-promise');
 
 export default class GitHubSigleUserDetail extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-           
+           userData : [],
         }
     }
 
-    componentDidMount(){
-        console.log("componentDidMount")
+    handleFetchData = (query) => {
+        if(query !== ""){
+           return getApi(`https://api.github.com/search/users?q=${query}`).then((res)=>{
+                if(res.data){
+                    if(res.data.items.length > 0){
+                        res.data.items.map((i)=>{
+                            i['label'] = i.login;
+                            i['value'] = i.id;
+                            return i;
+                        })
+                    }
+                }
+                return res.data.items
+            })
+            .catch(error => console.log(error))
+        }
     }
 
-    componentWillMount(){
-        console.log("componentWillMount")
+    handleSelect = (el) => {
+        this.setState({
+            userData : el
+        },()=>console.log(this.state.userData))
     }
 
     render(){
         return (
-            <div>git single user</div>
+            <React.Fragment>
+                <div className="main">
+                    <div className="todo-app" style={{float:"left",width:"20%"}}>
+                    <AsyncSelect
+                        isMulti={true}
+                        placeholder="search user name"
+                        value={this.state.userData}
+                        loadOptions={debounce(this.handleFetchData.bind(this), 800)}
+                        onChange={this.handleSelect}
+                        />    
+                    </div>
+                    {this.state.userData &&
+                        <React.Fragment>
+                            {this.state.userData.map((i)=>{
+                                return(
+                                    <div className="git_multi_user" key={i.id }>
+                                        <p><b>Name: </b>{i.login}</p>
+                                        <img className="git_img" src={i.avatar_url} alt="" />
+                                    </div>
+                                )
+                            })}
+                        </React.Fragment>
+                    }
+                </div>
+            </React.Fragment>
         )
     }
 }   
